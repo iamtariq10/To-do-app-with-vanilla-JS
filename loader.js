33cd36730b2ea7c80121b5c81109a34416ddded3
Mohammad Tariq <m.faixal16212@gmail.com>
2021-03-05 16:53:18 +0500

    To do app completed

--- loader.js
+++ loader.js
@@ -5,6 +5,16 @@ let toDoContainer = document.getElementById('toDoContainer');
 
 addBtn.addEventListener('click',function(){
     var paragraph = document.createElement('p');
+    paragraph.classList.add('p');
     paragraph.innerText = inputField.value;
     toDoContainer.appendChild(paragraph);
+    inputField.value = "";
+
+    paragraph.addEventListener('click',function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+
+    paragraph.addEventListener('dblclick',function(){
+        toDoContainer.removeChild(paragraph);
+    })
 })
